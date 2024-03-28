7f362d92e4807590b72c8599e55b8ae9307d47bb
Tunghohin <houhin0809@gmail.com>
2024-03-28 23:27:43 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -5,17 +5,6 @@
 #include <spdlog/sinks/rotating_file_sink.h>
 #include <spdlog/spdlog.h>
 
-struct walala {
-    int a = 2;
-    int b = 3;
-    char const* data = "palala";
-};
-
-auto operator<<(std::ostream& os, walala const& obj) -> std::ostream& {
-    return os << "walala: a: " << obj.a << ", b: " << obj.b
-              << ", c: " << obj.data;
-}
-
 int main() {
     auto logger = sgimg::log::LoggerImpl("mainlog");
     logger.info("Now: {}", 233);
